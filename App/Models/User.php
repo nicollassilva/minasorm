ca3ed7781d3317bcd8002b3b7b97adc6a7ffe79f
Nicollas <lyod.hp@gmail.com>
2021-05-07 12:18:57 -0300

    Fixing the files

--- App/Models/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-use MinasORM\Database;
-
-class User extends Database {
-    protected $primary = 'id';
-}
--- getTooling.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = @file_get_contents('http://localhost/tooling.php');
-$file = str_replace(['[', ']'], '\'', $file);
-$file = str_replace('(', '[', $file);
-$file = str_replace('Array', '', $file);
-$file = str_replace(')', '],', $file);
-
-echo $file;
